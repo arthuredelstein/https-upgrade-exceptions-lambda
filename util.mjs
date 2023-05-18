699b9521741d70fd6f54af17042d08fc96e4a76f
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-17 17:21:17 -0700

    util.mjs

---
+++ util.mjs
@@ -0,0 +1,25 @@
+import AWS from 'aws-sdk';
+
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+const callback = (resolve, reject) => (err, data) => err ? reject(err) : resolve(data);
+
+export const sendToSQS = (url, messageObject) => {
+  const params = {
+    // Remove DelaySeconds parameter and value for FIFO queues
+    DelaySeconds: 0,
+    MessageBody: JSON.stringify(messageObject),
+    QueueUrl: url
+  };
+  return new Promise((resolve, reject) => sqs.sendMessage(params, callback(resolve, reject)));
+};
+
+const dyanmo = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-11-05'});
+
+export const writeToDynamoDB = (url, item) => {
+  const params = {
+    TableName: 'https-results',
+    Item: item
+  };
+  return new Promise((resolve, reject) => dyanmo.put(params, callback(resolve, reject)));
+};
