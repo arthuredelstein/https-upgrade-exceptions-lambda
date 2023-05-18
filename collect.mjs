1782ffb1c58ccbaf4a46030caaa8eb99af4de769
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-17 17:20:14 -0700

    write results to dynamodb

--- collect.mjs
+++ collect.mjs
@@ -1,3 +1,12 @@
+import { writeToDynamoDB } from "./util.mjs";
+
+const resultsDB = "arn:aws:dynamodb:us-west-1:275005321946:table/https-results";
+
 export const handler = async (event, context) => {
   console.log(event);
+  for (const record of event.Records) {
+    const data = record.body;
+    const result = await writeToDynamoDB(resultsDB, data);
+    console.log(result);
+  }
 };
