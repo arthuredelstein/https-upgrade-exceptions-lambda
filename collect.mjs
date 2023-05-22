f57ebb12edf6732011298ea89cbf384b7ca6e082
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-21 23:02:11 -0700

    make collect.mjs work

--- collect.mjs
+++ collect.mjs
@@ -4,9 +4,14 @@ const resultsDB = "arn:aws:dynamodb:us-west-1:275005321946:table/https-results";
 
 export const handler = async (event, context) => {
   console.log(event);
-  for (const record of event.Records) {
-    const data = record.body;
-    const result = await writeToDynamoDB(resultsDB, data);
-    console.log(result);
+  if (event.Records) {
+    for (const record of event.Records) {
+      const data = JSON.parse(record.body);
+      const result = await writeToDynamoDB(resultsDB, data);
+      console.log({result});
+    }
+  } else {
+    const result = await writeToDynamoDB(resultsDB, event);
+    console.log({result});
   }
 };
