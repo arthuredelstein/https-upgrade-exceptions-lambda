1d01d155216b379dd7b38d0dcd87c0019be0497f
Arthur Edelstein <arthuredelstein@gmail.com>
2023-05-25 19:48:02 -0700

    bundle s3.mjs

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,6 @@
 rm -rf build
 rm ./test.zip
 mkdir -p build
-cp -r index.mjs util.mjs collect.mjs feed.mjs brave/ build/
+cp -r index.mjs util.mjs collect.mjs feed.mjs s3.mjs brave/ build/
 cd build/
 zip -r ../test.zip *
