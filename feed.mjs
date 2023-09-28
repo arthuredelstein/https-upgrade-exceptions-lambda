be5c09a9b0cddd863d197844873a157e9b526995
Arthur Edelstein <aedelstein@brave.com>
2023-09-28 16:33:39 -0700

    save snapshot of tranco domains

--- feed.mjs
+++ feed.mjs
@@ -1,5 +1,6 @@
 import { get as getTrancoData } from './brave/tranco.js';
-import { sendBatchToSQS } from './util.mjs';
+import { sendBatchToSQS, putText } from './util.mjs';
+import fsPromise from 'node:fs/promises';
 
 const domainQueue = 'https://sqs.us-west-1.amazonaws.com/275005321946/domain-queue';
 
@@ -19,23 +20,34 @@ const chunk = (array, n) => {
   return chunks;
 };
 
-const getChunkedDomains = async (total, chunkSize) => {
-  const { domains } = await getTrancoData(total);
-  return chunk(domains, chunkSize);
-};
-
-const sendDomainsToSQS = async (domains) => {
+const sendDomainsToSQS = async (domains, timeStamp, numScreenshots = 1) => {
   const resultPromises = [];
-  const timeStamp = new Date().toISOString().slice(0, 19).replace(/[-:]/g, '');
   for (const batch of domains) {
-    const resultPromise = sendBatchToSQS(domainQueue, batch.map(domain => ({ domain, timeStamp })));
+    const resultPromise = sendBatchToSQS(domainQueue, batch.map(domain => ({ domain, timeStamp, numScreenshots })));
     resultPromises.push(resultPromise);
   }
   const results = await Promise.allSettled(resultPromises);
-  console.log("batches sent: ", results.filter(r => r.status === "fulfilled").length);
+  console.log({timeStamp}, "batches sent: ", results.filter(r => r.status === "fulfilled").length);
 };
 
-export const handler = async (event, context) => {
-  const domains = await getChunkedDomains(event.count, 10);
-  await sendDomainsToSQS(domains);
+const saveDomainListToBucket = async (domains, timestamp) => {
+  await putText(`domainListSnapshots/${timestamp}`, domains.join("\n"));
 };
+
+const createTimeStamp = () => new Date().toISOString().slice(0, 19).replace(/[-:]/g, '');
+
+export const handler = async (event, context) => {
+  const { domains } = await getTrancoData(event.count);
+  const timestamp = createTimeStamp();
+  await saveDomainListToBucket(domains, timestamp)
+  const chunkedDomains = chunk(domains, 10);
+  await sendDomainsToSQS(chunkedDomains, timestamp);
+};
+
+export const followUpRun = async (filename) => {
+  const raw = JSON.parse(await fsPromise.readFile(filename));
+  const finalDomainList = raw.insecureParking.map(x => x.split("/").slice(-1)[0]);
+  console.log(finalDomainList.slice(0,3), "...");
+  const chunkedFinalDomainList = chunk(finalDomainList, 10);
+  await sendDomainsToSQS(chunkedFinalDomainList, createTimeStamp(), 5);
+}
