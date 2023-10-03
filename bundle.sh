ba632874380a6f12f77e173da31a7cdbb4e4d588
Arthur Edelstein <aedelstein@brave.com>
2023-10-03 10:38:30 -0700

    add produce.mjs to bundle

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,6 @@
 rm -rf build
 rm ./test.zip
 mkdir -p build
-cp -a index.mjs util.mjs feed.mjs analysis.mjs brave build/
+cp -a index.mjs util.mjs feed.mjs analysis.mjs produce.mjs brave build/
 cd build/
 zip -r ../test.zip *
